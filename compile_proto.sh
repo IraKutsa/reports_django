9201e6707adf938298e7a1908c4da63a91dfd267
Iryna Kutsa <iryna.kutsa@vakoms.com>
2023-06-08 13:03:36 +0300

    Proto files generation fixed

--- compile_proto.sh
+++ compile_proto.sh
@@ -2,4 +2,5 @@
 
 cd protocol_buffers || exit
 python -m grpc_tools.protoc -I../proto --python_out=. --pyi_out=. --grpc_python_out=. ../proto/employee_report.proto
+sed -i ./*_pb2_grpc.py -e 's/^import [^ ]*_pb2/from . \0/'
 cd - || exit
